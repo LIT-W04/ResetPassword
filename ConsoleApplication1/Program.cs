25cafdda488d1b80076fb4ae786c056e172384c5
Avrumi Friedman <a.friedman07@gmail.com>
2017-08-22 21:59:15 -0400

    Added some more text

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -18,7 +18,7 @@ namespace ConsoleApplication1
                 "XKsYHgKmi0Zu/buwv6pPTAB7VU3OT0vSwaHd5/IDNUs=");
             if (doesMatch)
             {
-                Console.WriteLine("Right password!");
+                Console.WriteLine("Right password! we're very happy for you!!");
             }
             else
             {
